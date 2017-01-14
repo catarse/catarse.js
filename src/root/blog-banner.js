4d1d31c3d72397e4cece6540eec89014b0a6e7a5
sushant12 <sussyoung9@gmail.com>
2017-01-14 09:48:07 +0545

    adding support for medium blog

--- src/root/blog-banner.js
+++ src/root/blog-banner.js
@@ -20,12 +20,12 @@ const blogBanner : Component = {
                 [
                     m('.u-text-center',
                         [
-                            m('a[href=\'http://blog.catarse.me\'][target=\'blank\']',
+                            m('a[href=\'https://medium.com/@myjvnepal\'][target=\'blank\']',
                                 m('img.u-marginbottom-10[alt=\'Icon blog\'][src=\'/assets/icon-blog.png\']')
                             ),
                             m('.fontsize-large.u-marginbottom-60.text-success',
-                                m('a.link-hidden-success[href=\'http://blog.catarse.me\'][target=\'__blank\']',
-                                    'Blog do Catarse'
+                                m('a.link-hidden-success[href=\'https://medium.com/@myjvnepal\'][target=\'__blank\']',
+                                    'JVN Blogs'
                                 )
                             )
                         ]
@@ -33,12 +33,12 @@ const blogBanner : Component = {
                     m('.w-row', _.map(ctrl.posts(), (post) => {
                         return m('.w-col.w-col-4.col-blog-post',
                             [
-                                m(`a.link-hidden.fontweight-semibold.fontsize-base.u-marginbottom-10[href="${post[1][1]}"][target=\'__blank\']`, post[0][1]),
-                                m('.fontsize-smaller.fontcolor-secondary.u-margintop-10', m.trust(`${h.strip(post[6][1].substr(0, 130))}...`))
+                                m(`a.link-hidden.fontweight-semibold.fontsize-base.u-marginbottom-10[href="${post[2][1]}"][target=\'__blank\']`, post[0][1]),
+                                m('.fontsize-smaller.fontcolor-secondary.u-margintop-10', m.trust(post[1][1]))
                             ]
                         );
                     })),
-                    ctrl.error() ? m('.w-row', m('.w-col.w-col-12.u-text-center', 'Erro ao carregar posts...')) : ''
+                    ctrl.error() ? m('.w-row', m('.w-col.w-col-12.u-text-center', 'Error loading posts...')) : ''
                 ]
             )
         );
