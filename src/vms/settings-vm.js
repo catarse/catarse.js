c0488cda94446092ddabb486ad3ae8a62b8202ac
Adriano Benin <adriano.benin@gmail.com>
2017-04-10 11:33:48 -0300

    get values from rails

--- src/vms/settings-vm.js
+++ src/vms/settings-vm.js
@@ -1,12 +1,5 @@
-const settingsVM = {
-    blog_url: 'http://blog.catarse.me',
-    catarse_fee: 0.13,
-    faq_url: 'http://suporte.catarse.me/',
-    base_url: 'https://www.catarse.me',
-    host: 'www.catarse.me',
-    support_forum: 'http://suporte.catarse.me/',
-    company_name: 'Catarse',
-    base_domain: 'catarse.me'
-};
+const root = document.getElementById('catarse_bootstrap'),
+    data = root && root.getAttribute('data-settings');
+const settingsVM = JSON.parse(data);
 
 export default settingsVM;
